e2c10a9a52ea28828d558470738631d7344c6ca0
Sirisha Aluri <sirisha.aluri@ge.com>
2016-03-31 11:34:24 -0700

    Added whole set of browsers

--- wct.conf.js
+++ wct.conf.js
@@ -1,15 +1,13 @@
 module.exports = {
-
-    verbose: true,
+    verbose: false,
     plugins: {
         local: {
             browsers: ['chrome', 'firefox']
         },
         sauce: {
-          disabled: false,
-          "browsers": [
-          
-          {
+             "disabled" : false,
+             "browsers": [
+             {
           "browserName": "internet explorer",
           "platform": "Windows 7",
           "version": "10"
@@ -22,8 +20,33 @@ module.exports = {
          {
           "browserName": "microsoftedge",
           "platform": "Windows 10"
-},
          
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 10",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8",
+          "version": "10"
+        },
+         {
+          "browserName": "firefox",
+          "platform": "Windows 8.1",
+          "version": "44"
+        },
+         {
+          "browserName": "chrome",
+          "platform": "Windows 8.1",
+          "version": "48"
+        },
          {
           "browserName": "safari",
           "platform": "OS X 10.10",
@@ -37,8 +60,8 @@ module.exports = {
           "version": "48"
         },
              
-
-        {
+           
+{
           "browserName": "safari",
           "platform": "OS X 10.11",
           "version": "9"
@@ -60,18 +83,14 @@ module.exports = {
           "deviceName": "iPad 2",
           "deviceOrientation": "portrait"
           
-        },
-        {
-          "browserName": "iphone",
-          "platform": "OS X 10.10",
-          "version": "9.2",
-          "deviceName": "iPad 2",
-          "deviceOrientation": "portrait"
-          
         }
-
+        
         
       ]
+             
+           
+            
+            
         }
     }
 };
